f579ef1ce5dfcafd875476cbb1eac6faa29e8024
Martynas <martynasjank@gmail.com>
2020-02-08 18:23:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Uzduotis</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
+        <header>
+            <nav>
+                <div class="innerT">
+                    <img class="logo" src="images/logo.png">
+                    <ul class="rightSide">
+                        <li>Features</li>
+                        <li>About</li>
+                        <li>Pricing</li>
+                        <li>Reviews</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="hContent">
+                <div class="innerB">
+                    <h4>Insert your picture here</h4>
+                    <div class="leftSide">
+                        <img class="logo" src="images/logobig.png">
+                        <h5>Modern axure template for beautiful prototypes</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                            euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+                            accumsan et viverra justo commodo.</p>
+                        <button>Download</button>
+                    </div>
+                    <div class="rightSide">
+                        <h6>Try Your <span>FREE</span> Trial Today</h6>
+                        <form>
+                            <input type="text" placeholder="Name">
+                            <input type="email" placeholder="Email">
+                            <input type="password" placeholder="Password">
+                        </form>
+                        <button>Get Started</button>
+                    </div>
+                </div>
+            </div>    
+</header>
+
+        <section class="social">
+            <div class="inner">
+                <div class="text">
+                    <h4>Social Media</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Aenean euismod bibendum laoreet.</p>
+                </div>
+                <div class="icons">
+                    <div class="ic ic1"></div>
+                    <div class="ic ic2"></div>
+                    <div class="ic ic3"></div>
+                    <div class="ic ic4"></div>
+                    <div class="ic ic5"></div>
+                    <div class="ic ic6"></div>
+                    <div class="ic ic7"></div>
+                </div>
+            </div>
+</section>
+
+        <section class="feat">
+            <div class="inner">
+                <div class="tabs">
+                    <div class="tab active">TAB 1</div>
+                    <div class="tab">TAB 2</div>
+                    <div class="tab">TAB 3</div>
+                </div>
+                <div class="tab_content">
+                    <h3>Tabs with soft transitioning effect.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+                        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra 
+                        justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque 
+                        penatibus et magnis dis parturient montes.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+                        bibendum laoreet.</p>
+                    <button>Download</button>
+                </div>
+                <div class="tab_image">
+                    <img src="images/tabimage.png">
+                </div>
+            </div>    
+</section>
+
+        <section class="sublist">
+            <div class="inner">
+                <div class="leftSide">
+                    <div class="picture">
+                        <span>Insert your picture here</span>
+                    </div>
+                </div>
+                <div class="rightSide">
+                    <h5>Sub list section</h5>
+                    <p class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
+                        euismod bibendum laoreet. Proin gravida dolor sit amet lacus 
+                        accumsan et viverra justo commodo.</p>
+                    <ul>
+                        <li>
+                            <h6>Title</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aenean euismod bibendum laoreet.</p>
+                        </li>
+                        <li>
+                            <h6>Title</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aenean euismod bibendum laoreet. Proin gravida dolor 
+                                sit amet lacus accumsan et viverra justo commodo.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>       
+        </section>
+
+        <section class="standard">
+            <div class="inner">
+                <div class="leftSide">
+                    <h5>Standard Picture Section</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
+                        euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan 
+                        et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis 
+                        natoque penatibus et magnis dis parturient montes.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+                        bibendum laoreet.</p>
+                </div>
+                <div class="rightSide">
+                     <div class="picture">
+                        <span>Insert your picture here</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="awesome">
+            <div class="inner">
+                <h1>WHY THIS IS AWESOME</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div class="third">
+                    <img src="images/one.png">
+                    <h3>Thoughtful Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Aenean euismod bibendum laoreet. Proin gravida 
+                        dolor sit amet lacus accumsan et viverra.</p>
+                </div>
+                <div class="third">
+                    <img src="images/two.png">
+                    <h3>Well Crafted</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Aenean euismod bibendum laoreet. Proin gravida 
+                        dolor sit amet lacus accumsan et viverra.</p>
+                </div>
+                <div class="third">
+                    <img src="images/three.png">
+                    <h3>Easy to Customize</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Aenean euismod bibendum laoreet. Proin gravida 
+                        dolor sit amet lacus accumsan et viverra.</p>
+                </div>
+            </div>
+        </section>
+        
+        <section class="pricing">
+            <div class="inner">
+                <h1>PRICING OPTIONS</h1>
+                <p class="top">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div class="third">
+                    <div class="price">
+                        <h2>Basic</h2>
+                        <div class="number">
+                            $<span>0</span>
+                        </div>
+                        <p>Free for Life</p>
+                    </div>
+                    <div class="benefits">
+                        <ul>
+                            <li>1 GB OF SPACE</li>
+                            <li>10GB OF BANDWIDTH</li>
+                            <li>3 WEBSITES</li>
+                            <li>BASIC CUSTOMIZATION</li>
+                            <li>WORDPRESS INTEGRATION</li>
+                            <li>EMAIL SUPPORT</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="third">
+                    <div class="price">
+                        <h2>Professional</h2>
+                        <div class="number">
+                            $<span>19</span>
+                        </div>
+                        <p>Monthly Payment</p>
+                        <div class="orange">
+                            OUT MOST POPULAR
+                        </div>
+                    </div>
+                    <div class="benefits">
+                        <ul>
+                            <li>5 GB OF SPACE</li>
+                            <li>50GB OF BANDWIDTH</li>
+                            <li>12 WEBSITES</li>
+                            <li>ADVANCED CUSTOMIZATION</li>
+                            <li>WORDPRESS INTEGRATION</li>
+                            <li>EMAIL SUPPORT</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="third">
+                    <div class="price">
+                        <h2>Enterprise</h2>
+                        <div class="number">
+                            $<span>70</span>
+                        </div>
+                        <p>Monthly Payment</p>
+                    </div>
+                    <div class="benefits">
+                        <ul>
+                            <li>UNLIMITED SPACE</li>
+                            <li>UNLIMITED BANDWIDTH</li>
+                            <li>100 WEBSITES</li>
+                            <li>ADVANCED CUSTOMIZATION</li>
+                            <li>WORDPRESS INTEGRATION</li>
+                            <li>24/7 CUSTOMER SUPPORT</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="customers">
+            <div class="inner">
+                <h2>WHAT OUT CUSTOMERS ARE SAYING</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div class="third">
+                    <blockquote>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. Doloribus 
+                        accusamus expedita repellat similique 
+                        odio aspernatur ex, architecto eaque 
+                        quo suscipit.</blockquote>
+                    <div class="person">
+                        <img class="picture" src="images/avatar.png">
+                        <div class="text">
+                            <h3>Jeremy H.</h3>
+                            <h6>Manager</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="third">
+                    <blockquote>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. Doloribus 
+                        accusamus expedita repellat similique 
+                        odio aspernatur ex, architecto eaque 
+                        quo suscipit.</blockquote>
+                    <div class="person">
+                        <img class="picture" src="images/avatar.png">
+                        <div class="text">
+                            <h3>John S.</h3>
+                            <h6>Freelancer</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="third">
+                    <blockquote>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. Doloribus 
+                        accusamus expedita repellat similique 
+                        odio aspernatur ex, architecto eaque 
+                        quo suscipit.</blockquote>
+                    <div class="person">
+                        <img class="picture" src="images/avatar.png">
+                        <div class="text">
+                            <h3>Susan W.</h3>
+                            <h6>Photographer</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="design">
+            <div class="inner">
+                <h3>STYLISH AXURE DESIGN</h3>
+                <p>Use the sections you need, remove the ones you don't.  Create gorgeous prototypes faster than ever!</p>
+                <button>Download</button>
+                <div class="insert">Insert your picture here</div>
+            </div>
+        </section>
+        
+        <section class="contact">
+            <div class="inner">
+                <h2>CONTACT US</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <form>
+                    <div class="leftSide">
+                        <input type="text" placeholder="Name">
+                        <input type="email" placeholder="Email">
+                        <input type="text" placeholder="Subject">
+                    </div>
+                    <div class="rightSide">
+                        <input type="text" placeholder="Message">
+                    </div>
+                    <input type="submit" value="Send Message">
+                </form>
+            </div>
+        </section>
+        
+        <footer>
+            <div class="inner">
+            <div class="icons">
+                <div class="ic ic1"></div>
+                <div class="ic ic2"></div>
+                <div class="ic ic3"></div>
+                <div class="ic ic4"></div>
+                <div class="ic ic5"></div>
+                <div class="ic ic6"></div>
+                <div class="ic ic7"></div>
+            </div>
+            <div class="copyright">
+                <span>&copy; 2015 Axure Themes</span>    
+            </div>
+            </div>
+        </footer>
+        
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
